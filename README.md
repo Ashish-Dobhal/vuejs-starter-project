71250f218e4637f6da2299f2b79ff4f165474839
Ashish Dobhal <adobhal@ia.zone>
2023-07-26 14:59:14 +0100

    Added nav mebu items and practised various attributes and directives

--- README.md
+++ README.md
@@ -1,3 +1,113 @@
+## Vuejs online playground
+[VueJs Online playground](https://play.vuejs.org/)
+## Vue JS Important concepts
+
+- built in directives: v-text v-html v-if v-show v-else v-else-if
+
+- if else if else code block
+
+```html
+<div v-if=" type === 'A'">A</div>
+<div v-else-if=" type === 'B'">B</div>
+<div v-else-if=" type === 'C'">C</div>
+<div v-else>Not A/B/C</div>
+```
+- for loop
+
+```html
+    <div v-for="item in employees" :key="item.id">
+      <li>{{ item.name }}</li>
+   </div>
+```
+
+- Very important read: https://vuejs.org/guide/essentials/list.html#maintaining-state-with-key
+- GOTCHA:
+-  It is recommended to provide a key attribute with v-for whenever possible, unless the iterated DOM content is simple (i.e. contains no components or stateful DOM elements), or you are intentionally relying on the default behavior for performance gains.
+
+- for loop with a component
+
+```html
+<todo :for="(todo,index) in todos"
+    :key="todo.id"
+    :propTitle="todo.title"
+    @todoEvent="methodInComponent(event)">
+</todo>
+```
+OR
+
+```html
+<template v-for="todo in todos" :key="todo.name">
+  <li>{{ todo.name }}</li>
+</template>
+```
+OR
+
+```html
+<MyComponent v-for="item in items" :key="item.id" />
+```
+
+- computed properties vs methods
+- computed are cached and lazy evaluated.
+- looks like a computed gotcha: https://dev.to/linusborg/vue-when-a-computed-property-can-be-the-wrong-tool-195j
+
+- class attribute
+```html
+<div
+  class="static"
+  :class="{ active: isActive, 'text-danger': hasError }">
+</div>
+```
+OR 
+==============================================================
+```js
+data() {
+  return {
+    classObject: {
+      active: true,
+      'text-danger': false
+    }
+  }
+}
+```
+ We can also bind to a computed property that returns an object. This is a common and powerful pattern:
+ ```js data() {
+  return {
+    isActive: true,
+    error: null
+  }
+},
+
+computed: {
+  classObject() {
+    return {
+      active: this.isActive && !this.error,
+      'text-danger': this.error && this.error.type === 'fatal'
+    }
+  }
+}
+```
+
+```html
+<div class="static active" :class="classObject"></div>
+<div :class="[isActive, errorClass]"></div>
+
+<div :class="[isActive ? activeClass : '', errorClass]"></div>
+```
+
+for inline styles
+
+```html
+<div :style="{ color: activeColor, fontSize: fontSize + 'px' }"></div>
+<div :style="styleObject"></div>
+<div :style="[baseStyles, overridingStyles]"></div>
+```
+==============================================================
+
+
+
+
+
+
 # job-search
 
 This template should help get you started developing with Vue 3 in Vite.
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,14 +1,44 @@
 <template>
-  <header class="w-full text-sm ">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <!-- <a v-bind:href="url" class="flex h-full items-center text-xl">{{ company }}</a> -->
         <!-- recommeded by vue style guide -->
-        <a v-bind:href="url" class="flex h-full items-center text-xl">{{ company }}</a>
-        <button class="bg-blue-600 m-4 absolute right-0 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign In</button>
-      </div> 
+        <a v-bind:href="url" target="_blank" class="flex h-full items-center text-xl" :text="company"></a>
+        <!-- <div v-if="showDescription" class="ml-5 mt-0 flex h-full items-center text-sm">
+          <i>a Ashish Ltd Company.</i>
+        </div> -->
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li :class="['h-full', index !== 0 ? 'ml-9' : '']" v-for="(navItem, index) in navItems"
+              :key="navItem.title">
+              <!-- :class="{'ml-9': index!==0" this works too for single properties -->
+              <a class="flex h-full items-center py-2.5"> {{ navItem.title }}</a>
+            </li>
+            <button class="absolute right-0 m-4 rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700">
+              Sign In
+            </button>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </header>  
+  </header>
+
+  <div class="relative ml-10 mt-20 flex h-16 w-full flex-row space-x-4 align-middle">
+    <h2 class="">Action Panel</h2>
+    <!-- Toggle btn code: https://codepen.io/lhermann/pen/EBGZRZ -->
+    <button class="h-8 rounded bg-green-600 px-2 font-bold text-white hover:bg-green-700"
+      @click="showDescription = !showDescription">
+      Toggle Me
+    </button>
+  </div>
+  <div class="relative ml-10 mt-20 flex h-16 w-full flex-col space-x-4 align-middle">
+    <h2>For Lop tryouts</h2>
+    <div v-for="item in employees" :key="item.id">
+      <li>{{ item.name }}</li>
+    </div>
+    <!-- Toggle btn code: https://codepen.io/lhermann/pen/EBGZRZ -->
+  </div>
 </template>
 
 <script>
@@ -17,8 +47,28 @@ export default {
   data() {
     return {
       company: "Dobbs Diaries",
-      url: "https://careers.google.com"
+      url: "https://careers.google.com",
+      styledCompany: `<b>Dobbs Diaries</b>`,
+      showDescription: false,
+      employees: [
+        { id: 2, name: "Ashish" },
+        { id: 1, name: "Nupur" }
+      ],
+      navItems: [
+        { title: "Teams", url: "www.google.com" },
+        { title: "Location", url: "www.google.com" },
+        { title: "Life at Dobbs Diaries", url: "www.google.com" },
+        { title: "How we Hire", url: "www.google.com" },
+        { title: "Students", url: "www.google.com" },
+        { title: "Jobs", url: "www.google.com" }
+      ]
+    }
+  },
+  methods: {
+    isFirstElement(index) {
+      return index !== 0
     }
   }
-};
+
+}
 </script>
