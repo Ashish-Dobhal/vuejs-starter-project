94c64889b500d4022378ec207f9c9bef588159da
Ashish Dobhal <adobhal@ia.zone>
2023-07-27 16:35:16 +0100

    lesson 120: register global components for fontawesome lib

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue"
 import "./index.css"
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+
 // GOTCHA installed the vscode plugin vetur for this to work. Not sure though
 import App from "@/App.vue"
+library.add(faSearch)
 
-createApp(App).mount("#app")
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app")
