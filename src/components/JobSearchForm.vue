d6c1f93babe81a47e61dd26c2674ad66b578a90b
Ashish Dobhal <adobhal@ia.zone>
2023-08-01 10:38:14 +0100

    refactor: updated job search form. replaced naive 2 way binding with v-model directive based two way binding

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -4,15 +4,15 @@
     <div class=" relative flex flex-1 flex-nowrap h-full text-base font-light">
       <div class="flex h-full flex-1 relative items-center pr-3"></div>
       <label class="absolute left-0 -top-9">Role</label>
-      <input type="text" :value="role" placeholder="Software engineer"
-        class="w-full mr-10 text-lg font-normal focus:outline-none" @input="updateRole" />
+      <input type="text" v-model="role" placeholder="Software engineer"
+        class="w-full mr-10 text-lg font-normal focus:outline-none" />
     </div>
     <span class="flex items-center h-full border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">in</span>
     <div class=" relative flex flex-1 flex-nowrap h-full text-base font-light">
       <div class="flex h-full flex-1 relative items-center pr-3"></div>
       <label class="absolute left-0 -top-9">Where?</label>
-      <input type="text" :value="location" placeholder="Los Angeles"
-        class="w-full mr-10 text-lg font-normal focus:outline-none" @input="location = $event.target.value" />
+      <input type="text" v-model="location" placeholder="London"
+        class="w-full mr-10 text-lg font-normal focus:outline-none" />
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl"></action-button>
   </form>
@@ -27,11 +27,6 @@ export default {
     return {
       role: "",
       location: ""
-    }
-  },
-  methods: {
-    updateRole(event) {
-      this.role = event.target.value
     }
   }
 }
