7aa72b47654368a6b775cb1c0b1886368eedbd07
Ashish Dobhal <adobhal@ia.zone>
2023-08-11 14:52:09 +0100

    feat(spotlight and slot props): added spotlight component highlighting the usage of slot props

---
+++ src/components/job-search/SpotLight.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+ul(class="mx-auto")
+  li(v-for="spotLight in spotLights" :key="spotLight.id") 
+    slot(:spotLight="spotLight")
+      h3 {{ spotlight.title }}
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'SpotLight',
+  data() {
+    return {
+      spotLights: []
+    }
+  },
+  async created() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL
+    const spotLightUrl = `${baseUrl}/spotlights`
+    const response = await axios.get(spotLightUrl)
+    this.spotLights = response.data
+  }
+}
+</script>
--- src/components/job-search/TheHero.vue
+++ src/components/job-search/TheHero.vue
@@ -1,27 +1,33 @@
-<template>
-  <main>
-    <section class="flex flex-col pb-20 pt-28 mx-auto my-0" style="max-width: 1440px;">
-      <div class="grid grid-cols-24">
-        <div class="col-span-1 col-start-1"></div>
-        <div class="col-span-10 col-start-2">
-          <the-headline />
-          <job-search-form />
-        </div>
-        <div class="col-start-12 col-span-2"></div>
-        <div class=" col-start-14 col-span-10 self-center justify-self-center">
-          <img class="opacity-60 w-100 h-100" src="../../../assets/hero_image_1.png" />
-        </div>
-      </div>
-    </section>
-  </main>
+<template lang="pug">
+main( class="flex h-screen flex-col")
+  section(class="pb-20 pt-28 mx-auto my-0" style="max-width: 1440px;")
+    div( class="grid grid-cols-24")
+      div( class="col-span-1 col-start-1")
+      div( class="col-span-10 col-start-2")
+        the-headline
+        job-search-form
+      div( class="col-start-12 col-span-2")
+      div( class=" col-start-14 col-span-10 self-center justify-self-center")
+        img( class="opacity-60 w-100 h-100" src="../../../../assets/hero_image_1.png")
+  spot-light(class="flex flex-row pb-16")
+    template(#default="slotProps")
+      router-link(to="/job/results" class="mx-5 flex flex-col h-96 w-72 rounded-lg border bg-brand-gray-2")
+        img(class="object-contain" :src="slotProps.spotLight.imgLocal")
+        div(class="mt-3 h-48 px-6 py-4")
+          h3(class="text-lg font-medium") {{ slotProps.spotLight.title }}
+          p(class="mt-3 text-sm") {{ slotProps.spotLight.description }}
+        router-link(to="/job/results" class="px-6 pb-4 text-sm text-brand-blue-1") See jobs
+
 </template>
 
 <script>
 import TheHeadline from "@/components/job-search/TheHeadline.vue"
 import JobSearchForm from '@/components/job-search/JobSearchForm.vue'
+import SpotLight from "@/components/job-search/SpotLight.vue"
+
 export default {
   name: "TheHero",
-  components: { TheHeadline, JobSearchForm },
+  components: { TheHeadline, JobSearchForm, SpotLight },
   beforeCreate() {
     console.log("componet abt to be created")
   },
